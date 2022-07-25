7f6d1efa70cdfc3fe84ce2e791573c7c711e403c
Trevor <trevor.a.bauer@dhhs.nh.gov>
2022-07-24 22:11:14 -0400

    Changed rw_visual scripts to continue with loop 	modified:   rw_visual.py

--- rw_visual.py
+++ rw_visual.py
@@ -1,9 +1,15 @@
 import matplotlib.pyplot as plt
-from random_walk2 import RandomWalk
+from random_walk import RandomWalk
 
-# Make a random walk, and plot the points
-rw=RandomWalk(50000)
-rw.fill_walk()
+# Keep making new walks, as long as the program is active. 
+while True:
+    # Make a new random walk, and plot the points.
+    rw=RandomWalk(50000)
+    rw.fill_walk()
 
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    plt.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
+
+    keep_running = input("Make another walk (y/n)?: ").lower()
+    if not keep_running.startswith('y'):
+        break
