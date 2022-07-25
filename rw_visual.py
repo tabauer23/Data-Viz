8ce957daf16dde26ca800e64cef77a05680c656c
Trevor <trevor.a.bauer@dhhs.nh.gov>
2022-07-24 22:22:53 -0400

    Make map prettier 	modified:   rw_visual.py

--- rw_visual.py
+++ rw_visual.py
@@ -4,10 +4,15 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is active. 
 while True:
     # Make a new random walk, and plot the points.
-    rw=RandomWalk(50000)
+    rw=RandomWalk(500000)
     rw.fill_walk()
 
-    plt.scatter(rw.x_values, rw.y_values, s=15)
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+
+    # Emphasize the first and last points
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
     plt.show()
 
     keep_running = input("Make another walk (y/n)?: ").lower()
